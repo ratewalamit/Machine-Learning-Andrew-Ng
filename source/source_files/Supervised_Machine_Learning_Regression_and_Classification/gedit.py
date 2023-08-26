fac77a86e0479d2b02684a9102f166d7162a7c44
Amit Kumar <ratewalamit@gmail.com>
2023-08-27 00:00:59 +0530

    fix path in Supervised.ipynb

---
+++ source/source_files/Supervised_Machine_Learning_Regression_and_Classification/gedit.py
@@ -0,0 +1,3 @@
+home_path = str(Path.home())
+proj_path=home_path+"/my_web/Machine-Learning-Andrew-Ng/source/source_files/Supervised_Machine_Learning_Regression_and_Classification"
+sys.path.append(f"{proj_path}/week3/C1W3A1/")
